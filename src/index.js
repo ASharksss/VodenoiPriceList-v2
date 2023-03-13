962d589c29b9c2312a1f126266b03f402265b49c
ASharkss <alsukurbanalieva035@gmail.com>
2023-03-13 16:02:54 +0300

    category season

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-//axios.defaults.baseURL = 'https://market.vodenoi.shop/v1';
-axios.defaults.baseURL = 'http://localhost:5000/v1';
+axios.defaults.baseURL = 'https://market.vodenoi.shop/v1';
+//axios.defaults.baseURL = 'http://localhost:5000/v1';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
