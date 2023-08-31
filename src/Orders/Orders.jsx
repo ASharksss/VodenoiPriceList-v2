1d4e7fd4791871b87d8af80c5c3f12f8d9bdc2d6
ASharksss <alsukurbanalieva035@gmail.com>
2023-08-31 21:04:51 +0300

    initial commit

--- src/Orders/Orders.jsx
+++ src/Orders/Orders.jsx
@@ -4,11 +4,17 @@ import './orders.css'
 
 const Orders = () => {
   return (
-    <div className=''>
+    <div className='orders_wrapper'>
       <h1>Заказы</h1>
+        <select className='orders_select'>
+            <option>Новые</option>
+            <option>В работе</option>
+            <option>Завершен</option>
+        </select>
         <div className="orders">
             <NavLink className='history_order-wrapper'>
                 <div className='history_order-item row'>
+                    <p>Заказчик</p>
                     <p>29.08.23</p>
                     <p>38 товаров</p>
                     <p>37900</p>
