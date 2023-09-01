e9d47db9552d007a47651c3b8a618917be2d1933
ASharkss <alsukurbanalieva035@gmail.com>
2023-09-01 10:16:14 +0300

    initial commit

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -5,46 +5,44 @@ import HistoryOrder from "./HistoryOrder";
 import axios from "axios";
 
 const Profile = ({role}) => {
-    const [user, setUser] = useState([])
-    const [orders, setOrders] = useState([])
+  const [user, setUser] = useState([])
+  const [orders, setOrders] = useState([])
 
-    const getUserInfo = async () => {
-        const id = window.location.pathname.split('/')[2]
-        if (id !== undefined) {
-            axios.get('/auth/user/info/' + id, {
-                headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')}
-            }).then(res => {
-                if (res.data.serverStatus === 1) {
-                    setUser(res.data.user)
-                    setOrders(res.data.orders)
-                }
-            })
-        } else {
-            axios.get('/auth/user/info', {
-                headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')}
-            }).then(res => {
-                if (res.data.serverStatus === 1) {
-                    setUser(res.data.user)
-                    setOrders(res.data.orders)
-                }
-            })
+  const getUserInfo = async () => {
+    const id = window.location.pathname.split('/')[2]
+    if (id !== undefined) {
+      axios.get('/auth/user/info/' + id, {
+        headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')}
+      }).then(res => {
+        if (res.data.serverStatus === 1) {
+          setUser(res.data.user)
+          setOrders(res.data.orders)
         }
+      })
+    } else {
+      axios.get('/auth/user/info', {
+        headers: {'Authorization': 'Bearer ' + localStorage.getItem('token')}
+      }).then(res => {
+        if (res.data.serverStatus === 1) {
+          setUser(res.data.user)
+          setOrders(res.data.orders)
+        }
+      })
     }
+  }
 
-    useEffect(() => {
-        getUserInfo()
-    }, [])
+  useEffect(() => {
+    getUserInfo()
+  }, [])
 
-    return (
-        <div className='profile_wrapper'>
-            <button className='noBtn backBtn' onClick={() => window.history.back()}>Назад</button>
-            <ProfileCard user={user}/>
-            {
-                role === 'user' ? <HistoryOrder orders={orders}/> : ''
-            }
+  return (
+    <div className='profile_wrapper'>
+      <button className='noBtn backBtn' onClick={() => window.history.back()}>Назад</button>
+      <ProfileCard user={user}/>
+      <HistoryOrder orders={orders}/>
 
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Profile;
