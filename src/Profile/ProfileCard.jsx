baefaf36a3851f8a589a670394ae5cf5b78080f9
ASharkss <alsukurbanalieva035@gmail.com>
2023-09-06 09:26:20 +0300

    initial commit

--- src/Profile/ProfileCard.jsx
+++ src/Profile/ProfileCard.jsx
@@ -9,18 +9,17 @@ const ProfileCard = ({user}) => {
         <div className='input-row'>
           <input type="text" value={user.organization} placeholder='Полное наименование предприятия' className='card_profile-input' disabled/>
           <input type="text" value={user.fio} placeholder='Директор' className='card_profile-input' disabled/>
-          <input type="text" placeholder='ИНН' className='card_profile-input' disabled/>
+          <input type="text" value={user.inn} placeholder='ИНН' className='card_profile-input' disabled/>
           <input type="text" placeholder='ОГРН' className='card_profile-input' disabled/>
         </div>
 
         <div className='input-row'>
           <input type="text" placeholder='Юридический адрес' className='card_profile-input' disabled/>
           <input type="text" value={user.number} placeholder='Телефон' className='card_profile-input' disabled/>
-          <input type="text" placeholder='Город' className='card_profile-input' disabled/>
+          <input type="text" placeholder='Город' className='card_profile-input'  disabled/>
           <input type="email" value={user.email} placeholder='Электронная почта' className='card_profile-input' disabled/>
         </div>
        </div>
-
     </div>
   );
 };
